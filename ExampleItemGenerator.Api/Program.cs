89ba50d02c88c72fc2818ee926d1aad782b83ff5
Marcin Badurowicz <m@badurowicz.net>
2023-12-11 14:02:08 +0100

    refactor: more minimal API

--- ExampleItemGenerator.Api/Program.cs
+++ ExampleItemGenerator.Api/Program.cs
@@ -6,28 +6,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IItemGenerator>(new BogusItemGenerator());
-builder.Services.AddCors(o => o.AddDefaultPolicy(builder =>
-{
-	builder.AllowAnyOrigin()
-		   .AllowAnyMethod()
-		   .AllowAnyHeader();
-}));
+builder.Services.AddCors(
+    o =>
+        o.AddDefaultPolicy(builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        })
+);
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseCors();
 
-app.MapGet("/item", async (IItemGenerator generator) =>
-{
-	return await generator.Generate();
-})
-.WithName("GetRandomItem")
-.WithOpenApi();
+app.MapGet(
+        "/item",
+        async (IItemGenerator generator) => await generator.Generate()
+    )
+    .WithName("GetRandomItem")
+    .WithOpenApi();
 
 app.Run();
